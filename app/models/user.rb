11022d38f569b044f15a62b3d70a7cb70dc35237
Thomas George <tfjgeorge@gmail.com>
2012-08-06 01:05:56 -0400

    authlogic

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :karma, :username, :news
   has_many :news
+  acts_as_authentic
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120806031010) do
+ActiveRecord::Schema.define(:version => 20120806044120) do
 
   create_table "news", :force => true do |t|
     t.integer  "user_id"
@@ -25,8 +25,11 @@ ActiveRecord::Schema.define(:version => 20120806031010) do
     t.string   "email"
     t.string   "username"
     t.float    "karma"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",        :null => false
+    t.datetime "updated_at",        :null => false
+    t.string   "crypted_password"
+    t.string   "password_salt"
+    t.string   "persistence_token"
   end
 
 end
