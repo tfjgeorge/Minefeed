183ed2d329a563748aa5c844271ef07611a125ef
Thomas George <tfjgeorge@gmail.com>
2012-08-05 22:32:03 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Minefeed::Application.config.secret_token = '8e01275a8b38cfd1d4d8dd27ab473475f0594b2d3b2e4345288ef970f2593de91c1b2cef846fe1975986963d9df879c3ccc312225bce7bddce8468fbd40e4834'
