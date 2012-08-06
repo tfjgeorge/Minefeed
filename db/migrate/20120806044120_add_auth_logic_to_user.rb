410bed0fbad321862dfd004d85696ee1604489cf
Thomas George <tfjgeorge@gmail.com>
2012-08-06 01:06:26 -0400

    authlogic

---
+++ db/migrate/20120806044120_add_auth_logic_to_user.rb
@@ -0,0 +1,7 @@
+class AddAuthLogicToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :crypted_password, :string
+    add_column :users, :password_salt, :string
+    add_column :users, :persistence_token, :string
+  end
+end
